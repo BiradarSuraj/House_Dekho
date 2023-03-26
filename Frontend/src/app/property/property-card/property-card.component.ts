3079c1fdc90ad03a836a041180ff7fae6e5b9d1d
Suraj Biradar <biradarsuraj03@gmail.com>
2023-03-26 23:16:29 +0530

    Added property list and aligned the all cards

--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 
 @Component({
@@ -8,10 +8,6 @@ import { Component } from "@angular/core";
   styleUrls:['property-card.component.css']
 })
 export class PropertyCardComponent {
-  property:any = {
-    "Id":1,
-    "name":"Birla House",
-    "type":"House",
-    "price":12000
-  }
+  @Input() property :any
+
 }
--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit {
+  properties: Array<any> = [
+    {
+      Id: 1,
+      name: 'Birla House',
+      type: 'House',
+      price: 12000,
+    },
+    {
+      Id: 2,
+      name: 'Mannat Villa',
+      type: 'Villa',
+      price: 120000,
+    },
+    {
+      Id: 3,
+      name: 'AAA House',
+      type: 'House',
+      price: 17000,
+    },
+    {
+      Id: 4,
+      name: 'ZZZ House',
+      type: 'House',
+      price: 45000,
+    },
+    {
+      Id: 5,
+      name: 'Taj House',
+      type: 'House',
+      price: 690000,
+    },
+    {
+      Id: 6,
+      name: 'Star Duplex',
+      type: 'Duplex',
+      price: 1690000,
+    }
+  ];
 
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
